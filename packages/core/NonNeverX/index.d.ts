445adc9750f206db77bb3830314614dca172c1c4
wzc520pyfm <1528857653@qq.com>
2023-12-27 00:13:38 +0800

    feat(core): add NonNeverX type

---
+++ packages/core/NonNeverX/index.d.ts
@@ -0,0 +1,15 @@
+import { Duplicate } from "@unts-utility/shared";
+
+/**
+ * NonNeverX
+ * @description Get Union type of object property types, but filtered out never types.
+ * @example
+ *  type Prop = {
+ *      name: string;
+ *      age: never;
+ *      visible: boolean;
+ *  }
+ *  // Expect: string | boolean
+ *  type NonNeverXProp = NonNeverX<Prop>
+ */
+export type NonNeverX<T extends object> = Duplicate<T>[keyof T]
