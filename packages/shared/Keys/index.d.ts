b48d07d8cb9bc4eded01e8965cf5c54bbc43914b
wzc520pyfm <1528857653@qq.com>
2023-12-28 00:06:56 +0800

    chore(shared): [Keys] update description

--- packages/shared/Keys/index.d.ts
+++ packages/shared/Keys/index.d.ts
@@ -1,6 +1,14 @@
 
 /**
  * Keys
- * @description Get union type composed of object keys.
+ * @description Get union type of all keys in object type T.
+ * @example
+ *  type Prop = {
+ *      name: string;
+ *      age: number;
+ *      getName: () => string;
+ *  }
+ *  // Expect: 'name' | 'age' | 'getName'
+ *  type KeysProp = Keys<Prop>
  */
 export type Keys<T extends object> = keyof T
