dc3c533898584492cd80be7483434ea4d925190c
wzc520pyfm <1528857653@qq.com>
2023-12-28 21:13:54 +0800

    feat(core): add OmitByValue type

---
+++ packages/core/OmitByValue/index.d.ts
@@ -0,0 +1,22 @@
+import { NonNeverX } from "@unts-utility/core";
+
+/**
+ * OmitByValue
+ * @description From T remove a set of properties by value matching ValueType.
+ * @example
+ *  type Prop = {
+ *      foo: number;
+ *      bar: number | undefined;
+ *      faz: boolean;
+ *  }
+ *  // Expect: { bar: number | undefined; faz: boolean; }
+ *  type OmitByValueProp1 = OmitByValue<Prop, number>
+ *  // Expect: { faz: boolean; }
+ *  type OmitByValueProp2 = OmitByValue<Prop, number | undefined>
+ */
+export type OmitByValue<T, ValueType> = Omit<
+  T,
+  NonNeverX<{
+    [K in keyof T]: T[K] extends ValueType ? K : never;
+  }>
+>;
