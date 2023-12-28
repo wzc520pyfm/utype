796b3435110a3cc794f104963b163cace4d19b9d
wzc520pyfm <1528857653@qq.com>
2023-12-28 21:50:55 +0800

    feat(core): add PickByValueFuzzy type

---
+++ packages/core/PickByValueFuzzy/index.d.ts
@@ -0,0 +1,22 @@
+import { NonNeverX } from "@unts-utility/core";
+
+/**
+ * PickByValueFuzzy
+ * @description From T pick a set of properties by value fuzzy matching ValueType.
+ * @example
+ *  type Prop = {
+ *      foo: number;
+ *      bar?: number;
+ *      faz: boolean;
+ *  }
+ *  // Expect: { bar?: number; }
+ *  type PickByValueFuzzyProp1 = PickByValueFuzzy<Prop, undefined>
+ *  // Expect: { bar?: number; faz: boolean; }
+ *  type PickByValueFuzzyProp2 = PickByValueFuzzy<Prop, boolean | undefined>
+ */
+export type PickByValueFuzzy<T, ValueType> = Pick<
+  T,
+  NonNeverX<{
+    [K in keyof T]: Extract<T[K], ValueType> extends never ? never : K;
+  }>
+>;
