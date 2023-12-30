8662e70f664357aaf8a6de51ebd5be07cfda4983
wzc520pyfm <1528857653@qq.com>
2023-12-30 21:21:42 +0800

    test(shared): [Keys] add test case

---
+++ packages/shared/Keys/index.d.test.ts
@@ -0,0 +1,12 @@
+import { Keys, Equal, Expect } from "@utility-ts/shared";
+
+type Case1 = {
+  readonly name: string;
+  age: number;
+  visible: boolean;
+};
+type KeysCase1 = "name" | "age" | "visible";
+
+type cases = [
+  Expect<Equal<Keys<Case1>, KeysCase1>>,
+];
