9f97587ffc5862a08b7198918fb5c4f90718a471
wzc520pyfm <1528857653@qq.com>
2023-12-31 01:40:05 +0800

    feat(core): add KebabCase type

---
+++ packages/core/KebabCase/index.d.test.ts
@@ -0,0 +1,14 @@
+import type { KebabCase } from "@utility-ts/core";
+import { Equal, Expect } from "@utility-ts/shared";
+
+type cases = [
+  Expect<Equal<KebabCase<"FooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"fooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"foo-bar">, "foo-bar">>,
+  Expect<Equal<KebabCase<"foo_bar">, "foo_bar">>,
+  Expect<Equal<KebabCase<"Foo-Bar">, "foo--bar">>,
+  Expect<Equal<KebabCase<"ABC">, "a-b-c">>,
+  Expect<Equal<KebabCase<"-">, "-">>,
+  Expect<Equal<KebabCase<"">, "">>,
+  Expect<Equal<KebabCase<"😎">, "😎">>
+];
---
+++ packages/core/KebabCase/index.d.ts
@@ -0,0 +1,13 @@
+
+/**
+ * KebabCase
+ * @description Replace the camelCase or PascalCase string with kebab-case.
+ * @example
+ *  // Expect: "foo-bar-baz"
+ *  type FooBarBaz = KebabCase<"FooBarBaz">
+ */
+export type KebabCase<S extends string> = S extends `${infer S1}${infer S2}`
+  ? S2 extends Uncapitalize<S2>
+    ? `${Uncapitalize<S1>}${KebabCase<S2>}`
+    : `${Uncapitalize<S1>}-${KebabCase<S2>}`
+  : S;
