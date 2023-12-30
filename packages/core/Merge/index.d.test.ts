508b95acb0c02d686ac5dff3c24c8447380ce90c
wzc520pyfm <1528857653@qq.com>
2023-12-31 00:00:34 +0800

    feat(core): [Merge] add MergeLeft and MergeRight  - In Merge, If the keys overlap, its values should be merged into an union.

--- packages/core/Merge/index.d.test.ts
+++ packages/core/Merge/index.d.test.ts
@@ -1,4 +1,4 @@
-import type { Merge } from "@utility-ts/core";
+import type { Merge, MergeLeft, MergeRight } from "@utility-ts/core";
 import { Equal, Expect } from "@utility-ts/shared";
 
 type Case1Foo = {
@@ -16,20 +16,74 @@ type MergeCase1 = {
 };
 
 type Case2Foo = {
-  name?: string;
-  age?: boolean;
+  name: string;
+  age: boolean;
 };
 type Case2Bar = {
   age: number;
   sex: string;
 };
 type MergeCase2 = {
-  name?: string;
-  age: number;
+  name: string;
+  age: number | boolean;
   sex: string;
+};
+
+type Case3Foo = {
+  readonly name: string;
+  readonly tt?: string;
+  age?: boolean;
+};
+type Case3Bar = {
+  tt: string[];
+  age: number;
+  sex?: string;
+};
+type MergeCase3 = {
+  readonly name: string;
+  readonly tt?: string | string[];
+  age?: number | boolean;
+  sex?: string;
+};
+
+type Case4Foo = {
+  readonly name: string;
+  readonly tt?: string;
+  age: boolean;
+};
+type Case4Bar = {
+  tt: string[];
+  age?: number;
+  sex?: string;
+};
+type MergeLeftCase4 = {
+  readonly name: string;
+  readonly tt?: string;
+  age: boolean;
+  sex?: string;
+};
+
+type Case5Foo = {
+  readonly name: string;
+  readonly tt?: string;
+  age: boolean;
+};
+type Case5Bar = {
+  tt: string[];
+  age?: number;
+  sex?: string;
+};
+type MergeRightCase5 = {
+  readonly name: string;
+  tt: string[];
+  age?: number;
+  sex?: string;
 };
 
 type cases = [
   Expect<Equal<Merge<Case1Foo, Case1Bar>, MergeCase1>>,
-  Expect<Equal<Merge<Case2Foo, Case2Bar>, MergeCase2>>
+  Expect<Equal<Merge<Case2Foo, Case2Bar>, MergeCase2>>,
+  Expect<Equal<Merge<Case3Foo, Case3Bar>, MergeCase3>>,
+  Expect<Equal<MergeLeft<Case4Foo, Case4Bar>, MergeLeftCase4>>,
+  Expect<Equal<MergeRight<Case5Foo, Case5Bar>, MergeRightCase5>>,
 ];
--- packages/core/Merge/index.d.ts
+++ packages/core/Merge/index.d.ts
@@ -1,12 +1,65 @@
 import { Duplicate, Keys } from "@utility-ts/shared";
+import { ReadonlyKeys, PartialKeys } from "@utility-ts/core";
 
 /**
  * Merge
+ * @description Merge two types into a new type. If the keys overlap, its values should be merged into an union.
+ * @example
+ *  type Foo = {
+ *      name: string;
+ *      age: string;
+ *  }
+ *  type Bar = {
+ *      age: number;
+ *      sex: string;
+ *  }
+ *  // Expect: {
+ *  //     name: string;
+ *  //     age: number | string;
+ *  //     sex: string;
+ *  // }
+ *  type MergeResult = Merge<Foo, Bar>
+ */
+export type Merge<F extends object, S extends object> = Duplicate<
+  {
+    [K in Exclude<Keys<F> | Keys<S>, ReadonlyKeys<F> | ReadonlyKeys<S> | PartialKeys<F> | PartialKeys<S>>]: F[K & Keys<F>] | S[K & Keys<S>]
+  } & {
+    readonly [K in Exclude<ReadonlyKeys<F> | ReadonlyKeys<S>, PartialKeys<F> | PartialKeys<S>>]: F[K & Keys<F>] | S[K & Keys<S>];
+  } & {
+    readonly [K in Extract<ReadonlyKeys<F> | ReadonlyKeys<S>, PartialKeys<F> | PartialKeys<S>>]?: F[K & Keys<F>] | S[K & Keys<S>];
+  } & {
+    [K in Exclude<PartialKeys<F> | PartialKeys<S>, ReadonlyKeys<F> | ReadonlyKeys<S>>]?: F[K & Keys<F>] | S[K & Keys<S>];
+  }
+>;
+
+/**
+ * MergeLeft
+ * @description Merge two types into a new type. Keys of the first type overrides keys of the second type.
+ * @example
+ *  type Foo = {
+ *      name: string;
+ *      age: string;
+ *  }
+ *  type Bar = {
+ *      age: number;
+ *      sex: string;
+ *  }
+ *  // Expect: {
+ *  //     name: string;
+ *  //     age: string;
+ *  //     sex: string;
+ *  // }
+ *  type MergeLeftResult = MergeLeft<Foo, Bar>
+ */
+export type MergeLeft<F, S> = Duplicate<Omit<S, Keys<F>> & F>;
+
+/**
+ * MergeRight
  * @description Merge two types into a new type. Keys of the second type overrides keys of the first type.
  * @example
  *  type Foo = {
  *      name: string;
- *      age: number;
+ *      age: string;
  *  }
  *  type Bar = {
  *      age: number;
@@ -17,6 +70,6 @@ import { Duplicate, Keys } from "@utility-ts/shared";
  *  //     age: number;
  *  //     sex: string;
  *  // }
- *  type MergeResult = Merge<Foo, Bar>
+ *  type MergeRightResult = MergeRight<Foo, Bar>
  */
-export type Merge<F, S> = Duplicate<Omit<F, Keys<S>> & S>;
+export type MergeRight<F, S> = Duplicate<Omit<F, Keys<S>> & S>;
