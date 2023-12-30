a7b8d89b3f4c9c8bc2d755d9c9b5a3d5b0f787a5
wzc520pyfm <1528857653@qq.com>
2023-12-31 01:53:48 +0800

    feat(core): add SnakeCase type

---
+++ packages/core/SnakeCase/index.d.test.ts
@@ -0,0 +1,15 @@
+import type { SnakeCase } from "@utility-ts/core";
+import { Equal, Expect } from "@utility-ts/shared";
+
+type cases = [
+  Expect<Equal<SnakeCase<"hello">, "hello">>,
+  Expect<Equal<SnakeCase<"userName">, "user_name">>,
+  Expect<Equal<SnakeCase<"UserPwd">, "user_pwd">>,
+  Expect<Equal<SnakeCase<"getElementById">, "get_element_by_id">>,
+  Expect<
+    Equal<
+      SnakeCase<"getElementById" | "getElementByClassNames">,
+      "get_element_by_id" | "get_element_by_class_names"
+    >
+  >
+];
---
+++ packages/core/SnakeCase/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * SnakeCase
+ * @description Replace the camelCase or PascalCase string with snake_case.
+ * @example
+ *  // Expect: "foo_bar_baz"
+ *  type FooBarBaz = SnakeCase<"FooBarBaz">
+ */
+export type SnakeCase<S extends string> = S extends `${infer S1}${infer S2}`
+  ? S2 extends Uncapitalize<S2>
+    ? `${Uncapitalize<S1>}${SnakeCase<S2>}`
+    : `${Uncapitalize<S1>}_${SnakeCase<S2>}`
+  : S;
