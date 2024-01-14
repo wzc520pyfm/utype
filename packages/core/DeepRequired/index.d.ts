096db11ef6c2b4b2a94eb21c9980d5462cad1109
wzc520pyfm <1528857653@qq.com>
2024-01-14 23:07:50 +0800

    feat(core): add DeepRequired type

---
+++ packages/core/DeepRequired/index.d.ts
@@ -0,0 +1,36 @@
+import { Keys, OnlyObject } from "@utype/shared";
+import { NonUndefined } from "@utype/core";
+
+/**
+ * DeepRequired
+ * @description Make every parameter of an object - and its sub-objects recursively - required.
+ * @example
+ *  type Prop = {
+ *    x: {
+ *      a?: 1
+ *      b: 'hi'
+ *    },
+ *    y?: 'hey'
+ *  }
+ *  // Expect: {
+ *  //   x: {
+ *  //     a: 1,
+ *  //     b: 'hi'
+ *  //   }
+ *  //   y: 'hey'
+ *  // }
+ * type DeepRequiredProp = DeepRequired<Prop>
+ */
+// todo need support array
+export type DeepRequired<T> = T extends OnlyObject
+  ? _DeepRequiredObject<T>
+  : T;
+
+/** @private */
+export interface _DeepRequiredArray<T>
+  extends Array<DeepRequired<NonUndefined<T>>> {}
+
+/** @private */
+export type _DeepRequiredObject<T> = {
+  [P in Keys<T>]-?: DeepRequired<NonUndefined<T[P]>>;
+};
